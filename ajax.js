edb7eb6e4f870c814db53e82237858c7c216e9ff
Kelvur <Osorio10.92@gmail.com>
2017-08-24 18:14:55 +0200

    First commit.

---
+++ ajax.js
@@ -0,0 +1,18 @@
+
+const DEFAULT_SETTINGS = {
+	method	:	'POST',
+	dataType:	'json',
+	error	:	default_error
+}
+
+function wrapper_ajax(url, data, callback, options = {}){
+	var settings = $.extend({}, DEFAULT_SETTINGS, options);
+	settings.url = url;
+	settings.data = data;
+	settings.success = callback;
+	$.ajax(settings);
+}
+
+function default_error(jqXHR, status, error){
+	console.error(''.concat(error, ' ', status));
+}
